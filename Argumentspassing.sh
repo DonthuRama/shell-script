f41b096950532bd49216a64a0c3b42c487b923fd
donthurama <donthuramakrishna@gmail.com>
2024-01-19 15:06:55 +0530

    arguments script

---
+++ Argumentspassing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo " $PERSON1: hello $PERSON2,good morning"
+echo "$PERSON2 :hi $PERSON1 ,very good morinig"
+echo "$PERSON1 : how are you $PERSON2"
+echo "$PERSON2 : am good how about you $PERSON1"
