af5a2259dc86fdba92a6e6f0ba9d3011e706445e
donthurama <donthuramakrishna@gmail.com>
2024-01-19 15:48:03 +0530

    arry script

--- arrysc.sh
+++ arrysc.sh
@@ -5,3 +5,5 @@ FRUITS=("Apple" "mango" "grape" "guva")
 echo "first fruitname is :${FRUITS[0]}"
 echo "secondfruitname is :${FRUITS[1]}"
 echo "third fruitname is :${FRUITS[2]}"
+
+echo "All fruits are:${FRUITS[@]}"
