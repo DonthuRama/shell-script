8e56af8e995c0cbc9f508d7a20e7963cfef97ce4
donthurama <donthuramakrishna@gmail.com>
2024-01-19 14:38:09 +0530

    varable script

---
+++ varables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo " ramesh: hello suresh,good morning"
+echo "suresh :hi ramesh ,very good morinig"
+echo "remesh : how are you suresh"
+echo "suresh : am good how about you ramesh"
