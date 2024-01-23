c99cbd7ccdc186ec1b27fe1f470e945a676b021c
donthurama <donthuramakrishna@gmail.com>
2024-01-23 15:51:46 +0530

    conditions script

--- conditions.sh
+++ conditions.sh
@@ -4,9 +4,9 @@ NUMBERS=$1
 
 if [ $NUMBERS -gt 100 ]
     then
-echo "Given number is $1 is greater than 100"
+echo "Given number  $1 is greater than 100"
 
 else
 
-echo "Given number is $1 is not greater than 100"
+echo "Given number  $1 is not greater than 100"
 fi
