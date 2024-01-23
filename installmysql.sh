b74b519d62f4ff82ea1dbd416bddacf17bdb3215
donthurama <donthuramakrishna@gmail.com>
2024-01-23 16:14:52 +0530

    installmy sql script

--- installmysql.sh
+++ installmysql.sh
@@ -4,9 +4,15 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-echo"yes u r notroot user proced with root user"
+
+   echo  "yes u r notroot user proced with root user"
+
 exit 1
+
 else
-echo"u r root userproceed with installation"
+
+ echo "u r root userproceed with installation"
+
 fi
+
 yum install mysql -y
